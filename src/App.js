25f5d399e57aad6980710556417572433756b68a
jsontype <jsontyper@gmail.com>
2023-07-09 15:17:58 +0900

    fix: alt 프롭을 줄거리로 변경

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function App() {
         <img
           className="movieImage"
           src={item.large_cover_image}
-          alt={item.title}
+          alt={item.summary}
         ></img>
       </div>
     )
