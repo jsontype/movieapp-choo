3fea700fdb6079fc199f141cd1c285034f5a4f35
吳珮甄 <mu@Polly.local>
2023-07-15 02:33:04 +0900

    feat: 영화 평점 추가

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import React, { useState, useEffect } from "react"
-import "./App.css"
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 export default function App() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
   // const url = "https://yts.mx/api/v2/list_movies.json"
-  const url = "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+  const url = "https://yts.mx/api/v2/list_movies.json?sort_by=rating";
 
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
       .then((json) => {
-        setMovies(json.data.movies)
-      })
-  }, [])
+        setMovies(json.data.movies);
+      });
+  }, []);
 
-  console.log(movies)
+  console.log(movies);
 
   const render = movies.map((item) => {
     return (
       <div key={item.id}>
         <div>{item.title}</div>
+        <div
+          className={
+            item.rating >= 9 ? "good" : item.rating >= 7 ? "soso" : "bad"
+          }
+        >
+          평점 : {item.rating} / 10
+        </div>
         <img
           className="movieImage"
           src={item.large_cover_image}
           alt={item.summary}
         ></img>
       </div>
-    )
-  })
+    );
+  });
 
   return (
     <>
       <h1>무비앱</h1>
       <div>{render}</div>
     </>
-  )
+  );
 }
