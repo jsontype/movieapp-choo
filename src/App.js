e3eff81891b8e8e8ee005c516ed84c2b04685de9
吳珮甄 <mu@polly.lan>
2023-07-16 12:09:11 +0900

    fix: no semicolon

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react"
+import "./App.css"
 
 export default function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([])
 
   // const url = "https://yts.mx/api/v2/list_movies.json"
-  const url = "https://yts.mx/api/v2/list_movies.json?sort_by=rating";
+  const url = "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
 
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
       .then((json) => {
-        setMovies(json.data.movies);
-      });
-  }, []);
+        setMovies(json.data.movies)
+      })
+  }, [])
 
-  console.log(movies);
+  console.log(movies)
 
   const render = movies.map((item) => {
     return (
@@ -37,13 +37,13 @@ export default function App() {
           alt={item.summary}
         ></img>
       </div>
-    );
-  });
+    )
+  })
 
   return (
     <>
       <h1>무비앱</h1>
       <div>{render}</div>
     </>
-  );
+  )
 }
