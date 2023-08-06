40e3d553ea6cec48ecde4391f9a9b50fabce06a3
jsontype <jsontyper@gmail.com>
2023-08-06 14:14:58 +0900

    feat: lint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import { BrowserRouter } from "react-router-dom"
-import i18n from "./i18n"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import i18n from './i18n'
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
     <App i18n={i18n} />
-  </BrowserRouter>
+  </BrowserRouter>,
 )
