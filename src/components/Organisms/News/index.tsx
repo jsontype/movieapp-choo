2e0eecc402bc56c27c8cf40b0ce3bdd3d205f93f
chooseongyeop <csy6197@gmail.com>
2023-08-10 21:42:03 +0900

    feat:NewsApp mui테이블적용

--- src/components/Organisms/News/index.tsx
+++ src/components/Organisms/News/index.tsx
@@ -1,6 +1,13 @@
-import { useEffect, useState, memo, useMemo } from "react"
+import { useEffect, useState, memo } from "react"
 import styles from "./style.module.scss"
 import { useTranslation } from "react-i18next"
+import TableContainer from "@mui/material/TableContainer"
+import Table from "@mui/material/Table"
+import TableHead from "@mui/material/TableHead"
+import TableRow from "@mui/material/TableRow"
+import TableCell from "@mui/material/TableCell"
+import TableBody from "@mui/material/TableBody"
+import Paper from "@mui/material/Paper"
 
 type NewsItemProps = {
   url: string
@@ -20,45 +27,38 @@ function News() {
       .then((json) => setNewsList(json))
   }, [])
 
-  const render = useMemo(
-    () =>
-      newsList.map((news: NewsItemProps, index: number) => {
-        return (
-          <div key={index}>
-            <a
-              href={news.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.newsTitle}
-            >
-              {news.title}
-            </a>
-            <span
-              className={
-                styles[
-                  news.comments_count >= 100
-                    ? "newsComments_countHigh"
-                    : news.comments_count >= 50
-                    ? "newsComments_countLow"
-                    : "newsComments_count"
-                ]
-              }
-            >
-              {t("news:itemCommentsCount")}: {news.comments_count}
-            </span>
-            <div className={styles.newsUser}>
-              {t("news:itemUser")}: {news.user}
-            </div>
-          </div>
-        )
-      }),
-    [newsList, t]
-  )
-
   return (
     <>
       <div className={styles.title}>{t("news:title")}</div>
-      <div>{render}</div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="news table">
+          <TableHead>
+            <TableRow>
+              <TableCell>{t("news:itemTitle")}</TableCell>
+              <TableCell align="right">{t("news:itemCommentsCount")}</TableCell>
+              <TableCell align="right">{t("news:itemUser")}</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {newsList.map((news: NewsItemProps, index: number) => (
+              <TableRow key={index}>
+                <TableCell component="th" scope="row">
+                  <a
+                    href={news.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.newsTitle}
+                  >
+                    {news.title}
+                  </a>
+                </TableCell>
+                <TableCell align="right">{news.comments_count}</TableCell>
+                <TableCell align="right">{news.user}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
