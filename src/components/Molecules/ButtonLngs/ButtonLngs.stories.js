aeb28e650061451b36b9993bc76d675c7d08abe1
chooseongyeop <csy6197@gmail.com>
2023-08-20 12:09:09 +0900

    homework1

---
+++ src/components/Molecules/ButtonLngs/ButtonLngs.stories.js
@@ -0,0 +1,14 @@
+import ButtonLngs from ".";
+
+export default {
+  title: "Molecules/ButtonLngs", // 스토리북에서 보여질 카테고리와 이름
+  component: ButtonLngs, // 어떤 컴포넌트를 문서화할지 정의
+  // 이벤트를 스토리북 액션으로 바인딩
+};
+
+const Template = (args) => <ButtonLngs {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  onChangeLng: (language) => console.log(`Selected language: ${language}`),
+};
---
+++ src/modules/todos.js
@@ -0,0 +1,46 @@
+// 액션 타입
+const ADD_TODO = "todos/ADD_TODO";
+const TOGGLE_TODO = "todos/TOGGLE_TODO";
+const DELETE_TODO = "todos/DELETE_TODO";
+
+// 액션 생성 함수
+let key = 1; // todo 데이터에서 사용할 고유 id
+
+export const addTodo = (title) => ({
+  type: ADD_TODO,
+  todo: {
+    id: key++,
+    title,
+    completed: false,
+    userId: 1,
+  },
+});
+
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
+  id,
+});
+
+export const deleteTodo = (id) => ({
+  type: DELETE_TODO,
+  id,
+});
+
+// 초기값
+const initialState = [];
+
+// 리듀서 함수
+export default function todos(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, action.todo];
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, completed: !todo.completed } : todo,
+      );
+    case DELETE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+    default:
+      return state;
+  }
+}
